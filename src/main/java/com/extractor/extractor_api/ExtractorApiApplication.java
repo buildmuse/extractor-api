442b9d4b07a66055573c2f50fa42504e01f1009f
Preethi Panneerselvam <preethip21786@gmail.com>
2025-11-04 10:13:15 -0600

    Initial commit: extractor-api Spring Boot project

---
+++ src/main/java/com/extractor/extractor_api/ExtractorApiApplication.java
@@ -0,0 +1,13 @@
+package com.extractor.extractor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractorApiApplication.class, args);
+	}
+
+}
